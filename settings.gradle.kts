0e831201cdb8efb90985d38f52bf56ebef5d085b
김의현 <hyunkim6790@gmail.com>
2026-02-01 13:13:17 +0900

    build: Gradle 라이브러리 의존성 정의 수정
    - `libs.versions.toml`에서 `group`과 `name`을 사용하도록 의존성 표기법을 통일
    - `settings.gradle.kts`에서 사용하지 않는 카카오 라이브러리 그룹(`com.kakao.i`) 제거

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
 
-        // https://docs.gradle.org/current/userguide/filtering_repository_content.html?utm_source=chatgpt.com
+        // https://docs.gradle.org/current/userguide/filtering_repository_content.html
         // forRepository에 선언된 kakao 저장소는 filter에 선언된 라이브러리 그룹만 조회
         exclusiveContent {
             forRepository {
@@ -27,7 +27,6 @@ dependencyResolutionManagement {
             }
             filter {
                 includeGroup("com.kakao.sdk")
-                includeGroup("com.kakao.i")
             }
         }
     }

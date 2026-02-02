c6ca197f219e8a574fa191277b5512b9e49905b4
Ju Hyeok <zero5.two4@gmail.com>
2026-02-02 23:16:57 +0900

    feat(home): 앱바, 프로필 UI 구현

--- presentation/src/main/java/com/smtm/pickle/presentation/home/HomeScreen.kt
+++ presentation/src/main/java/com/smtm/pickle/presentation/home/HomeScreen.kt
@@ -1,19 +1,17 @@
 package com.smtm.pickle.presentation.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.smtm.pickle.presentation.designsystem.theme.PickleTheme
+import com.smtm.pickle.presentation.home.component.HomeProfile
+import com.smtm.pickle.presentation.home.component.HomeTopBar
 
 @Composable
 fun HomeScreen(
@@ -22,6 +20,8 @@ fun HomeScreen(
     onNavigateToLedgerDetail: (Long) -> Unit,
 ) {
     HomeContent(
+        monthlyTotalIncome = 1_000_000,
+        monthlyTotalExpense = 500_000,
         onNavigateToLedgerCreate = onNavigateToLedgerCreate,
         onNavigateToLedgerDetail = onNavigateToLedgerDetail
     )
@@ -29,27 +29,46 @@ fun HomeScreen(
 
 @Composable
 private fun HomeContent(
+    monthlyTotalIncome: Long,
+    monthlyTotalExpense: Long,
     onNavigateToLedgerCreate: () -> Unit,
     onNavigateToLedgerDetail: (Long) -> Unit,
 ) {
     Surface(
         modifier = Modifier.fillMaxSize(),
-        color = Color.Black.copy(0.5f)
+        color = PickleTheme.colors.background50,
     ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(PickleTheme.colors.background50),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = onNavigateToLedgerCreate) {
-                Text("가계부 쓰기")
+            item("top_bar") {
+                HomeTopBar()
             }
 
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Button(onClick = { onNavigateToLedgerDetail(1L) }) {
-                Text("가계부 상세")
+            item("profile") {
+                HomeProfile(
+                    badge = "뱃지명",
+                    nickname = "나의닉네임",
+                    income = monthlyTotalIncome,
+                    expense = monthlyTotalExpense,
+                )
             }
         }
+    }
+}
+
+@Preview
+@Composable
+private fun HomeContentPreview() {
+    PickleTheme {
+        HomeContent(
+            monthlyTotalIncome = 1000000,
+            monthlyTotalExpense = 200000,
+            onNavigateToLedgerCreate = {},
+            onNavigateToLedgerDetail = {},
+        )
     }
 }
---
+++ presentation/src/main/java/com/smtm/pickle/presentation/home/component/HomeTopBar.kt
@@ -0,0 +1,43 @@
+package com.smtm.pickle.presentation.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.smtm.pickle.presentation.designsystem.theme.PickleTheme
+import com.smtm.pickle.presentation.designsystem.theme.dimension.Dimensions
+
+@Composable
+fun HomeTopBar(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .background(PickleTheme.colors.base0)
+            .fillMaxWidth()
+            .height(Dimensions.appbarHeight)
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "LOGO")
+    }
+}
+
+@Preview(
+    name = "HomeTopBar - New Alarm",
+    showBackground = true,
+    widthDp = 360
+)
+@Composable
+private fun HomeTopBarPreview() {
+    PickleTheme {
+        HomeTopBar()
+    }
+}

e32a10e850e5b060bf433718bf844dc2a5fc0d29
김의현 <hyunkim6790@gmail.com>
2026-02-06 01:20:00 +0900

    feat(mypage): 닉네임 임시 저장 기능 구현
    - 마이페이지에 프로필 화면을 연결합니다.
    - `NicknameRepository`에 닉네임을 가져오는 `getNickname` Flow를 추가합니다.
    - 프로필 수정에 필요한 문자열 리소스를 추가합니다.
    - `TrailingIcon` 컴포저블에서 불필요한 수평 패딩을 제거합니다.

--- domain/src/main/java/com/smtm/pickle/domain/repository/NicknameRepository.kt
+++ domain/src/main/java/com/smtm/pickle/domain/repository/NicknameRepository.kt
@@ -1,9 +1,13 @@
 package com.smtm.pickle.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface NicknameRepository {
 
     suspend fun isNicknameAvailable(nickname: String): Boolean
 
     suspend fun saveNickname(nickname: String)
+
+    fun getNickname(): Flow<String>
 
 }

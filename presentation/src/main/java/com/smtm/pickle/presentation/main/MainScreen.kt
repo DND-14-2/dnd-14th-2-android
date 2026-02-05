c91f2c0f7d40e1964d6ebfa83a2a6403cdf3c179
Ju Hyeok <zero5.two4@gmail.com>
2026-02-05 13:25:59 +0900

    fix(expandable_fab): expandable fab 상태 관리 개선

--- presentation/src/main/java/com/smtm/pickle/presentation/main/MainScreen.kt
+++ presentation/src/main/java/com/smtm/pickle/presentation/main/MainScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -57,6 +58,10 @@ fun MainScreen(
     var bottomBarHeight by remember { mutableStateOf(0.dp) }
     var selectedDate by remember { mutableStateOf(LocalDate.now()) }
     val density = LocalDensity.current
+
+    LaunchedEffect(isHomeScreen) {
+        if (!isHomeScreen) isFabExpanded = false
+    }
 
     Box(modifier = Modifier.fillMaxSize()) {
         MainContent(

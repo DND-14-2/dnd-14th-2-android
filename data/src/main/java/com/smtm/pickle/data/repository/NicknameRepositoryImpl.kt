41961125976bb13efd1a47bb716232dafd8272fa
김의현 <hyunkim6790@gmail.com>
2026-02-13 00:20:46 +0900

    refactor(nickname): 닉네임 관리 로직 개선
    - `ProfileDataStore`를 추가하여 닉네임을 로컬에 저장하고 관찰할 수 있도록 변경합니다.
    - `NicknameRepository`에서 닉네임 조회 및 변경 시 API 호출과 함께 `ProfileDataStore`를 사용하도록 수정합니다.
    - `UserApi`에 프로필 정보를 가져오는 `getProfile()` API를 추가합니다.

--- data/src/main/java/com/smtm/pickle/data/repository/NicknameRepositoryImpl.kt
+++ data/src/main/java/com/smtm/pickle/data/repository/NicknameRepositoryImpl.kt
@@ -1,28 +1,29 @@
 package com.smtm.pickle.data.repository
 
+import com.smtm.pickle.data.source.local.datastore.ProfileDataStore
 import com.smtm.pickle.data.source.remote.api.UserApi
 import com.smtm.pickle.data.source.remote.model.user.NicknameRequest
 import com.smtm.pickle.domain.repository.NicknameRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class NicknameRepositoryImpl @Inject constructor(
-    private val userApi: UserApi
+    private val userApi: UserApi,
+    private val profileDataStore: ProfileDataStore
 ) : NicknameRepository {
-
-    private val _nicknameFlow = MutableStateFlow("닉네임")
 
     override suspend fun saveNickname(nickname: String) {
         userApi.changeNickname(NicknameRequest(nickname))
-        _nicknameFlow.value = nickname
+        profileDataStore.changeNickname(nickname)
     }
 
-    // TODO: 닉네임 조회 API 완료 후 연결
-    override suspend fun getNickname(): String = _nicknameFlow.value
+    override suspend fun getNickname(): String {
+        val nickname = userApi.getProfile().nickname
+        profileDataStore.changeNickname(nickname)
+        return nickname
+    }
 
-    override fun observeNickname(): Flow<String> = _nicknameFlow.asStateFlow()
+    override fun observeNickname(): Flow<String> = profileDataStore.observeNickname()
 }
--- data/src/main/java/com/smtm/pickle/data/source/remote/api/UserApi.kt
+++ data/src/main/java/com/smtm/pickle/data/source/remote/api/UserApi.kt
@@ -1,12 +1,18 @@
 package com.smtm.pickle.data.source.remote.api
 
 import com.smtm.pickle.data.source.remote.model.user.NicknameRequest
+import com.smtm.pickle.data.source.remote.model.user.ProfileResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface UserApi {
+
+    @GET("users/me")
+    suspend fun getProfile(): ProfileResponse
 
     @POST("users/me/nickname")
     suspend fun changeNickname(@Body request: NicknameRequest)
 
 }
+

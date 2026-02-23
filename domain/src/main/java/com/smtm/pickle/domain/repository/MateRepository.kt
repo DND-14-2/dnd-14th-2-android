40ac11838d9761074d71e27613260a04da21ff33
Ju Hyeok <zero5.two4@gmail.com>
2026-02-23 22:19:45 +0900

    feat: 친구 목록 API 연동 구현

--- domain/src/main/java/com/smtm/pickle/domain/repository/MateRepository.kt
+++ domain/src/main/java/com/smtm/pickle/domain/repository/MateRepository.kt
@@ -1,8 +1,11 @@
 package com.smtm.pickle.domain.repository
 
+import com.smtm.pickle.domain.model.mate.Mate
 import com.smtm.pickle.domain.model.mate.MateId
 
 interface MateRepository {
 
     suspend fun inviteMate(invitationCode: String): MateId
+
+    suspend fun getMates(): List<Mate>
 }

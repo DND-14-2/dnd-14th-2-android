9fd1a1d7de46c961a8db906a19f03fb31a3fff39
Ju Hyeok <zero5.two4@gmail.com>
2026-02-25 14:08:28 +0900

    refactor: NicknameRepository 기능을 UserRepository로 통합
    - NicknameRepository 및 NicknameRepositoryImpl 삭제
    - UserRepository 인터페이스에 nickname 관련 메서드(save, get, observe) 추가
    - UserRepositoryImpl에서 UserApi 및 ProfileDataStore를 사용하여 nickname 기능 구현
    - RepositoryModule에서 NicknameRepository 바인딩 제거 및 AuthRepository에 @Singleton 추가

--- data/src/main/java/com/smtm/pickle/data/di/RepositoryModule.kt
+++ data/src/main/java/com/smtm/pickle/data/di/RepositoryModule.kt
@@ -2,11 +2,9 @@ package com.smtm.pickle.data.di
 
 import com.smtm.pickle.data.repository.AuthRepositoryImpl
 import com.smtm.pickle.data.repository.LedgerRepositoryImpl
-import com.smtm.pickle.data.repository.NicknameRepositoryImpl
 import com.smtm.pickle.data.repository.UserRepositoryImpl
 import com.smtm.pickle.domain.repository.AuthRepository
 import com.smtm.pickle.domain.repository.LedgerRepository
-import com.smtm.pickle.domain.repository.NicknameRepository
 import com.smtm.pickle.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
@@ -19,15 +17,12 @@ import javax.inject.Singleton
 abstract class RepositoryModule {
 
     @Binds
+    @Singleton
     abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
 
     @Binds
     @Singleton
     abstract fun bindUserRepository(impl: UserRepositoryImpl): UserRepository
-
-    @Binds
-    @Singleton
-    abstract fun bindNicknameRepository(impl: NicknameRepositoryImpl): NicknameRepository
 
     @Binds
     @Singleton
--- data/src/main/java/com/smtm/pickle/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/smtm/pickle/data/repository/UserRepositoryImpl.kt
@@ -1,13 +1,20 @@
 package com.smtm.pickle.data.repository
 
 import com.smtm.pickle.data.source.local.datastore.PreferencesDataStore
+import com.smtm.pickle.data.source.local.datastore.ProfileDataStore
+import com.smtm.pickle.data.source.remote.api.UserApi
+import com.smtm.pickle.data.source.remote.model.user.NicknameRequest
 import com.smtm.pickle.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class UserRepositoryImpl @Inject constructor(
-    private val preferencesDataStore: PreferencesDataStore
+    private val userApi: UserApi,
+    private val preferencesDataStore: PreferencesDataStore,
+    private val profileDataStore: ProfileDataStore
 ) : UserRepository {
 
     override suspend fun setOnboardingCompleted(completed: Boolean) {
@@ -25,4 +32,17 @@ class UserRepositoryImpl @Inject constructor(
     override suspend fun getFirstLogin(): Boolean {
         return preferencesDataStore.isFirstLogin().first()
     }
+
+    override suspend fun saveNickname(nickname: String) {
+        val changedNickname = userApi.changeNickname(NicknameRequest(nickname)).nickname
+        profileDataStore.changeNickname(changedNickname)
+    }
+
+    override suspend fun getNickname(): String {
+        val profile = userApi.getProfile()
+        profileDataStore.changeNickname(profile.nickname)
+        return profile.nickname
+    }
+
+    override fun observeNickname(): Flow<String?> = profileDataStore.observeNickname()
 }

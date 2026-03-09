ae372c833c5de47664fac0bf195eb911162a3acf
김의현 <hyunkim6790@gmail.com>
2026-03-10 00:15:10 +0900

    feat: 사용자 프로필 응답 모델을 RemoteUser로 변경
    - RemoteUser: id, nickname, level, invitationCode를 포함하는 신규 모델 추가
    - UserApi: getProfile의 반환 타입을 ProfileResponse에서 RemoteUser로 변경

--- data/src/main/java/com/smtm/pickle/data/source/remote/api/UserApi.kt
+++ data/src/main/java/com/smtm/pickle/data/source/remote/api/UserApi.kt
@@ -2,7 +2,7 @@ package com.smtm.pickle.data.source.remote.api
 
 import com.smtm.pickle.data.source.remote.model.user.NicknameRequest
 import com.smtm.pickle.data.source.remote.model.user.NicknameResponse
-import com.smtm.pickle.data.source.remote.model.user.ProfileResponse
+import com.smtm.pickle.data.source.remote.model.user.RemoteUser
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -10,7 +10,7 @@ import retrofit2.http.POST
 interface UserApi {
 
     @GET("users/me")
-    suspend fun getProfile(): ProfileResponse
+    suspend fun getProfile(): RemoteUser
 
     @POST("users/me/nickname")
     suspend fun changeNickname(@Body request: NicknameRequest): NicknameResponse

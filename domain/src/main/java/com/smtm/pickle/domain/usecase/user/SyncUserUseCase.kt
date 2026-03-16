5c9e4bff04e3d4810195daed000c24becc479331
김의현 <hyunkim6790@gmail.com>
2026-03-17 00:26:24 +0900

    refactor(SyncUserUseCase): invoke 반환 타입을 Result<String?>으로 변경
    - userRepository.getNickname()의 nullable 반환값 처리를 위해 타입 수정

--- domain/src/main/java/com/smtm/pickle/domain/usecase/user/SyncUserUseCase.kt
+++ domain/src/main/java/com/smtm/pickle/domain/usecase/user/SyncUserUseCase.kt
@@ -7,7 +7,7 @@ import javax.inject.Inject
 class SyncUserUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(): Result<String> = runSuspendCatching {
+    suspend operator fun invoke(): Result<String?> = runSuspendCatching {
         userRepository.getNickname()
     }
 }

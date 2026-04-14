de85d04c2b6200758d189c96e80dadbbf57e2027
Ju Hyeok <zero5.two4@gmail.com>
2026-04-15 04:34:12 +0900

    design(login): 데모 로그인 버튼을 PickleButton으로 교체

--- presentation/src/main/java/com/smtm/pickle/presentation/login/components/ButtonSection.kt
+++ presentation/src/main/java/com/smtm/pickle/presentation/login/components/ButtonSection.kt
@@ -11,9 +11,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -21,6 +19,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smtm.pickle.presentation.R
+import com.smtm.pickle.presentation.designsystem.components.button.PickleButton
 import com.smtm.pickle.presentation.designsystem.theme.PickleTheme
 import com.smtm.pickle.presentation.login.LoginUiState
 
@@ -86,19 +85,14 @@ fun ButtonSection(
             )
         }
 
-        Spacer(modifier = Modifier.height(20.dp))
+        Spacer(modifier = Modifier.height(11.dp))
 
-        TextButton(
+        PickleButton(
+            modifier = Modifier.fillMaxWidth(),
+            text = stringResource(R.string.login_demo_start),
             onClick = onDemoLogin,
             enabled = uiState !is LoginUiState.Loading,
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text(
-                text = stringResource(R.string.login_demo_start),
-                style = PickleTheme.typography.body4Medium,
-                color = PickleTheme.colors.gray500
-            )
-        }
+        )
     }
 }
 
